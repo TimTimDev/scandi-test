bbe3bc1106473b13e468b8d38d73f2d6cebf6c3d
TimTimDev <timo.pirn@gmail.com>
2022-02-17 18:03:35 +0200

    loop now works better

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ $price = "Price";
 $attribute = "Attribute";
 
 $products = array(
-                    "SKU"  => array("JS3F225", "JS3F226", "JS3F227"),
-                    "Name" => ["CyberPunk", "Rust", "LoL"],
-                    "Price" => [49.99, 19.99, 0],
-                    "Size" => [70000, 40000, 12000] 
+                    "SKU"  => ["JS3F225", "JS3F226", "JS3F227", "JS3F228"],
+                    "Name" => ["CyberPunk", "Rust", "LoL", "GTAV"],
+                    "Price" => [49.99, 19.99, 0, 39.99],
+                    "Size" => [70000, 40000, 12000, 80000] 
                 );
 
                     
@@ -17,13 +17,12 @@ require 'index.view.php';
 
 echo "<div class='product-list'>";
 
-foreach($products as $product) {
+for($x = 0; $x <= count($product); $x++) {
     echo "<div class='product'>";
     echo "<input type='checkbox' name='delete-checkbox' id='delete-checkbox'>";
-    echo "<span> $product[0] </span>";
-    echo "<span> $product[1] </span>";
-    echo "<span> $product[2] </span>";
-    echo "<span> $product[3] </span>";
+    foreach($products as $product) {
+        echo "<span> $product[$x] </span>";
+    }
     echo "</div>";
 }
 
