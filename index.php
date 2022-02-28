5b3a4937674d52883a38512ae048167856d2ca67
TimTimDev <timo.pirn@gmail.com>
2022-02-28 18:28:09 +0200

    Refactor

--- index.php
+++ index.php
@@ -1,40 +1,9 @@
 <?php
 
-$host = "d98707.mysql.zonevs.eu";
-$servername = "d98707_scandi";
-$username = "d98707_tester";
-$password = "";
+$database = require 'bootstrap.php';
 
-/* try {
-  $conn = new PDO("mysql:host=$host;dbname=$servername", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-} */
+$products = $database->selectAll('Products');
 
-$sku = "SKU";
-$name = "Name";
-$price = "Price";
-$attribute = "Attribute";
-
-$products = [
-              ["SKU"  => "JS3F225",
-              "Name" => "CyberPunk",
-              "Price" => 49.99,
-              "Size" => 70000], 
-              ["SKU"  => "JS3F226",
-              "Name" => "xD",
-              "Price" => 49,
-              "Size" => 70000], 
-              ["SKU"  => "JS3F227",
-              "Name" => "LoL",
-              "Price" => 0.99,
-              "Size" => 70000], 
-            ];
-
-                    
 if(isset($_POST["delete-product"]) && isset($_POST["delete-checkbox"]))
 {
   // var_dump(isset($_POST["delete-checkbox"]));
@@ -46,24 +15,7 @@ if(isset($_POST["delete-product"]) && isset($_POST["delete-checkbox"]))
 
 require 'index.view.php';
 
-/* for($x = 1; $x <= count($products); $x++) {
-    echo "<div class='product'>";
-    echo "<input type='checkbox' name='delete-checkbox' id='delete-checkbox'>";
-    foreach($products as $key => $value) {
-        echo "<span> $value </span>";
-    }
-    echo "</div>";
-}
- */
-
-
-
 // $checked = $_GET['delete-checkbox'];
 // foreach($checked as )
 
-
-
-// fix loop
-// Add functionality to buttons
 // Refactor & reorganize code
-// Connect DB
--- index.view.php
+++ index.view.php
@@ -8,24 +8,29 @@
     <title>Product List</title>
 </head>
 <body>
+    <header class="product-list-header">
+        <h1>Product List</h1>
+        <div class="header-btns">
+            <a href="product-add.php" class="btn"><button>ADD</button></a>
     <form action="index.php" method="POST">
-        <header class="product-list-header">
-            <h1>Product List</h1>
-            <div class="header-btns">
-                <a href="product-add.php" class="btn"><button>ADD</button></a>
-                <button name="delete-product" value="delete-product-btn" type="submit" id="delete-product-btn" class="btn">MASS DELETE</button>
-            </div>
-        </header>
+            <button name="delete-product" value="delete-product-btn" type="submit" id="delete-product-btn" class="btn">MASS DELETE</button>
+        </div>
+    </header>
 
         <div class="product-list">
 
         <?php  foreach($products as $i => $product){ ?>
             <div class="product">
-                    <input type='checkbox' name='delete-checkbox[]' class='delete-checkbox' value="<?= $i ?>">
-                    <span><?= $product["SKU"] ?></span>
-                    <span><?= $product["Name"] ?></span>
-                <span><?= $product["Price"] ?></span>
-                <span><?= $product["Size"] ?></span>
+                <input type='checkbox' name='delete-checkbox[]' class='delete-checkbox' value="<?= $i ?>">
+                <span><?= $product->sku ?></span>
+                <span><?= $product->name ?></span>
+                <span><?= $product->price ?></span>
+                <span><?= $product->attribute ?></span>
+
+                <!-- Possible type display check solution: -->
+                <!-- <span>${product.type == 0 ? 'Size: ' : product.type == 1 ? 'Weight: ' : 'Dimension: '}${product.attribute}</span> -->
+
+
             </div>
             <?php  } ?>            
             
